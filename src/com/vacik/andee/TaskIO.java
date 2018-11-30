a8f1fdaf7a15c52547a7586846022ffa21834c6c
Andee13 <39676479+Andee13@users.noreply.github.com>
2018-11-30 16:59:27 +0200

    start 6 practise v1.1

--- src/com/vacik/andee/TaskIO.java
+++ src/com/vacik/andee/TaskIO.java
@@ -1,4 +1,26 @@
 package com.vacik.andee;
 
+import java.io.*;
+
 public class TaskIO {
+    static void write(TaskList tasks, OutputStream out){
+       try {
+           DataOutputStream outputStream =  new DataOutputStream(out);
+           outputStream.writeInt(tasks.size());
+           for(int i = 0; i < tasks.size(); i++) {
+                outputStream.writeInt(tasks.getTask(i).getTitle().length());
+                outputStream.writeChars(tasks.getTask(i).getTitle());
+                outputStream.writeBoolean(tasks.getTask(i).isActive());
+                outputStream.writeInt(tasks.getTask(i).getRepeatInterval());
+                if(tasks.getTask(i).getRepeatInterval() != 0) {
+                    outputStream.writeLong();
+                } else {
+
+                }
+           }
+
+       } catch (IOException ex) {
+           System.out.println(ex);
+       }
+    }
 }

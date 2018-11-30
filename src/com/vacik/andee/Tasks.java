be0ba55d0872b69a4193304cf8e199e898d64812
Andee13 <39676479+Andee13@users.noreply.github.com>
2018-11-30 14:44:23 +0200

    finish 5 practise

--- src/com/vacik/andee/Tasks.java
+++ src/com/vacik/andee/Tasks.java
@@ -24,6 +24,7 @@ public class Tasks {
         if(start.after(end)) {
             throw new IllegalArgumentException("Unpropriate time");
         }
+
         Iterator<Task> iterator =  tasks.iterator();
         TaskList tasks_in = new LinkedTaskList();
         while(iterator.hasNext()){
@@ -40,28 +41,45 @@ public class Tasks {
     }
 
     public static SortedMap<Date, Set<Task>> calendar(Iterable<Task> tasks, Date start, Date end){
+        if(start == null || end == null){
+            throw new IllegalArgumentException("null argumet");
+        }
+        if(start.after(end)) {
+            throw new IllegalArgumentException("Unpropriate time");
+        }
+
         SortedMap<Date, Set<Task>> sortedMap =  new TreeMap<Date, Set<Task>>();
 
 
-        Iterable<Task> tasks_in = incoming(tasks, start, end);
-        Iterator<Task> iterator =  tasks_in.iterator();
+        /*Iterator<Task> iterator =  tasks.iterator();
+        TaskList tasks_in = new LinkedTaskList();
+        while(iterator.hasNext()){
+            Task task = iterator.next();
+            Date temp = task.nextTimeAfter(start);
+            if( temp != null && temp.after(start) ){
+                if ( temp.before(end) || temp.equals(end)) {
+                    tasks_in.add(task);
+                }
+            }
 
-        for(Task temp : tasks) { // for ( Iterator<Task> iter = tasks.iterator(); iter.hasNext(); iter.next() )
-            for(Date time = temp.nextTimeAfter(start); time != null; time = temp.nextTimeAfter(time))
-            {
+        }*/
+        Iterable<Task> tasks_in = incoming(tasks, start, end);
+        //Iterator<Task> iterator =  tasks_in.iterator();
+
+        for(Task temp : tasks_in) { // for ( Iterator<Task> iter = tasks.iterator(); iter.hasNext(); iter.next() )
+            for(Date time = temp.nextTimeAfter(start); time != null && time.after(start) && (time.equals(end)|| time.before(end)); time = temp.nextTimeAfter(time)){
                 if(sortedMap.containsKey(time)){
                     Set<Task> t = sortedMap.get(time);
                     t.add(temp);
-                    sortedMap.replace(time, t);
+                    //sortedMap.replace(time, t);
                 } else {
-                    Set<Task> set = new TreeSet<Task>();
+                    Set<Task> set = new LinkedHashSet<Task>();
                     set.add(temp);
                     sortedMap.put(time, set);
                 }
-
-
             }
         }
+
         return sortedMap ;
     }
 }
